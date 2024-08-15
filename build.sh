b96d98abaacee52bb7a6d1e7baba852ad30128ad
MohamedBsh <mohamed-amine.bousahih@doctrine.fr>
2024-08-15 16:59:04 +0200

    ⚛⚛⚛⚛

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 gcc -o common_molecular src/identification_common_molecular.c
 ./common_molecular data/data.txt
-emcc --emrun src/identification_common_molecular.c -s WASM=1 -o common_molecular.html
-emrun --no_browser --port 8080 common_molecular.html
